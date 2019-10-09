3ad725c35fd361fa19b74ea24214343608f6a21b
Suryateja <suryateja.gujju@gmail.com>
2019-10-09 12:07:18 +0530

    added Viewset to Profiles API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     ''' Serializes a name field for testing our API view'''
     name = serializers.CharField(max_length = 10)
-    
+    #date = serializers.DateField()
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import viewsets
 from profiles_api import serializers
 
 class HelloApiView(APIView):
@@ -13,8 +13,7 @@ class HelloApiView(APIView):
                 'Uses HTTP methods as functions(get,put,post,patch,delete)',
                 'Is similar to a traditional Django View',
                 'Gives you most control over your application logic',
-                'Is mapped manulaly to URLs'
-        ]
+                'Is mapped manulaly to URLs'        ]
 
         return Response({'message':'Hello','an_apiview':an_apiview})
 
@@ -33,7 +32,6 @@ class HelloApiView(APIView):
 
     def put(self,request,pk=None):
         ''' Handle updating an object'''
-
         return Response({'method':'PUT'})
 
 
@@ -45,3 +43,46 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message."""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message."""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
